f7625ac2bd51d383b029e7816e0ac736f3e761b0
aswsx <61579000+aswsx@users.noreply.github.com>
2020-08-17 00:21:07 +0600

    9.1. String [#343669]

--- src/ru/job4j/Main.java
+++ src/ru/job4j/Main.java
@@ -1,7 +1,8 @@
 package ru.job4j;
 
 public class Main {
-    public static void main(String [] args) {
+    @SuppressWarnings("checkstyle:NoWhitespaceAfter")
+    public static void main(String[] args) {
         System.out.println("Hello world");
     }
 }
