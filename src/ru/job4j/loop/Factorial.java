ce1cc73c8f42e74367417152c5f48a7a47619866
aswsx <61579000+aswsx@users.noreply.github.com>
2020-08-18 00:38:40 +0600

    6.0.5. Массивы в тестах. [#343643]

--- src/ru/job4j/loop/Factorial.java
+++ src/ru/job4j/loop/Factorial.java
@@ -3,15 +3,11 @@ package ru.job4j.loop;
 public class Factorial {
 
     public static int calc(int n) {
-        int result = 0;
-        if (n == 0) {
-            return 1;
-        } else {
-            result = 1;
-            for (int i = 1; i <= n; i++) {
-                result *= i;
-            }
-            return result;
+        int result = 1;
+        for (int i = 1; i <= n; i++) {
+            result *= i;
         }
+        return result;
     }
 }
+
