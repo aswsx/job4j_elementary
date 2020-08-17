fd64bd283ffd6be49030d44d0033b6a89021773e
aswsx <61579000+aswsx@users.noreply.github.com>
2020-08-17 14:59:40 +0600

    4.3.1. Оператор && [#343615]

--- src/ru/job4j/condition/Cinema.java
+++ src/ru/job4j/condition/Cinema.java
@@ -1,17 +1,34 @@
 package ru.job4j.condition;
 
 public class Cinema {
-    public static void access(int age) {
-        System.out.println("The age of the customer is : " + age);
-        if (age >= 18) {
-            System.out.println("Welcome to the cinema.");
+
+    public static void permission(boolean allowByParent, boolean hasMoney) {
+        if (allowByParent && hasMoney) {
+            System.out.println("I can go to the cinema.");
         } else {
-            System.out.println("It is not for you.");
+            System.out.println("I can't.");
         }
     }
 
     public static void main(String[] args) {
-        Cinema.access(21);
-        Cinema.access(16);
+        permission(true, true);
+        permission(true, false);
+        permission(false, true);
+        permission(false, false);
     }
+
+    //    public static void access(int age) {
+//        System.out.println("The age of the customer is : " + age);
+//        if (age >= 18) {
+//            System.out.println("Welcome to the cinema.");
+//        } else {
+//            System.out.println("It is not for you.");
+//        }
+//    }
+//
+//    public static void main(String[] args) {
+//        Cinema.access(21);
+//        Cinema.access(16);
+//    }
 }
+
