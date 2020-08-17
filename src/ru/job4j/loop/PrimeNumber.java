1366cf292a1a1406ff9973f6609ae4dd1d52a002
aswsx <61579000+aswsx@users.noreply.github.com>
2020-08-18 01:10:12 +0600

    5.6. Простые числа [#343715]

--- src/ru/job4j/loop/PrimeNumber.java
+++ src/ru/job4j/loop/PrimeNumber.java
@@ -4,15 +4,11 @@ public class PrimeNumber {
 
     public static int calc(int finish) {
         int count = 0;
-        if (finish == 1) {
-            return 0;
-        } else {
-            for (int i = 1; i <= finish; i++) {
-                if (CheckPrimeNumber.check(i)) {
-                    count++;
-                }
+        for (int i = 0; i <= finish; i++) {
+            if (CheckPrimeNumber.check(i)) {
+                count++;
             }
-            return count;
         }
+        return count;
     }
 }
