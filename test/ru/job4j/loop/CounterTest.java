cd0e90481e2c874f3fc610838c2ab693f9795d74
aswsx <61579000+aswsx@users.noreply.github.com>
2020-08-17 20:52:55 +0600

    5.0.1. Сумма чисел. [#343719]

---
+++ test/ru/job4j/loop/CounterTest.java
@@ -0,0 +1,23 @@
+package ru.job4j.loop;
+
+import org.junit.Test;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.*;
+
+public class CounterTest {
+
+    @Test
+    public void whenSumEvenNumbersFromOneToTenThenThirty() {
+        int rsl = Counter.sumByEven(1, 10);
+        int expected = 30;
+        assertThat(rsl, is(expected));
+    }
+
+    @Test
+    public void whenSumEvenNumbersFromFiveToTenThenTwentyFour() {
+        int rsl = Counter.sumByEven(5, 10);
+        int expected = 24;
+        assertThat(rsl, is(expected));
+    }
+}
