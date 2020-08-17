7d7d135b1f637791b32690343b1d99f6b94a58bf
aswsx <61579000+aswsx@users.noreply.github.com>
2020-08-17 09:53:53 +0600

    1.1. Входные данные в тесте [#343703]

---
+++ test/ru/job4j/calculate/X2Test.java
@@ -0,0 +1,64 @@
+package ru.job4j.calculate;
+
+import org.junit.Test;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.*;
+
+public class X2Test {
+
+    @Test
+    public void whenBCZero() {
+        int a = 10;
+        int b = 0;
+        int c = 0;
+        int x = 2;
+        int expected = 40;
+        int rsl = X2.calc(a, b, c, x);
+        assertThat(rsl, is(expected));
+    }
+
+    @Test
+    public void whenABCOne() {
+        int a = 1;
+        int b = 1;
+        int c = 1;
+        int x = 1;
+        int expected = 3;
+        int rsl = X2.calc(a, b, c, x);
+        assertThat(rsl, is(expected));
+    }
+
+    @Test
+    public void whenAZero() {
+        int a = 0;
+        int b = 1;
+        int c = 1;
+        int x = 1;
+        int expected = 2;
+        int rsl = X2.calc(a, b, c, x);
+        assertThat(rsl, is(expected));
+    }
+
+    @Test
+    public void whenCZero() {
+        int a = 1;
+        int b = 1;
+        int c = 0;
+        int x = 1;
+        int expected = 2;
+        int rsl = X2.calc(a, b, c, x);
+        assertThat(rsl, is(expected));
+    }
+
+    @Test
+    public void whenXZero() {
+        int a = 1;
+        int b = 1;
+        int c = 1;
+        int x = 0;
+        int expected = 1;
+        int rsl = X2.calc(a, b, c, x);
+        assertThat(rsl, is(expected));
+    }
+}
