ff6b64d5b74aadcd02fb5abe952006c65982e08f
aswsx <61579000+aswsx@users.noreply.github.com>
2020-08-17 09:27:22 +0600

    1. Модульные тесты. [#343698]

---
+++ test/ru/job4j/converter/ConverterTest.java
@@ -0,0 +1,25 @@
+package ru.job4j.converter;
+
+import org.junit.Test;
+import org.junit.Assert;
+
+import static org.junit.Assert.*;
+
+public class ConverterTest {
+
+    @Test
+    public void whenConvert140RblThen2Euro() {
+        int in = 140;
+        int expected = 2;
+        int out = Converter.rubleToEuro(in);
+        Assert.assertEquals(expected, out);
+    }
+
+    @Test
+    public void whenConvert240RblThen3Dlr() {
+        int in = 240;
+        int expected = 4;
+        int out = Converter.rubleToDollar(in);
+        Assert.assertEquals(expected, out);
+    }
+}
