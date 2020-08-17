ec6ccb8df886b8c70463c8a39cd17b204a5f9d65
aswsx <61579000+aswsx@users.noreply.github.com>
2020-08-18 00:09:11 +0600

    6.0.2. Заполнение массива. [#343641]

--- src/ru/job4j/array/ArrayDefinition.java
+++ src/ru/job4j/array/ArrayDefinition.java
@@ -1,7 +1,20 @@
 package ru.job4j.array;
 
 public class ArrayDefinition {
+
     short[] ages = new short[10];
     String[] surnames = new String[100500];
     float[] prices = new float[40];
+
+    public static void main(String[] args) {
+        String[] names = new String[4];
+        names[0] = "Vasya";
+        names[1] = "Fedya";
+        names[2] = "Vitya";
+        names[3] = "Misha";
+
+        for (int i = 0; i < names.length; i++) {
+            System.out.println(names[i]);
+        }
+    }
 }
