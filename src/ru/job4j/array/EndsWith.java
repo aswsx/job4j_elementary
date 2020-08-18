65240e14620626854d6626b4099b7889e5a6d637
aswsx <61579000+aswsx@users.noreply.github.com>
2020-08-18 13:29:14 +0600

    6.5.3. Сортировка выборкой. [#343635]

--- src/ru/job4j/array/EndsWith.java
+++ src/ru/job4j/array/EndsWith.java
@@ -3,13 +3,12 @@ package ru.job4j.array;
 public class EndsWith {
     public static boolean endsWith(char[] word, char[] post) {
         boolean result = true;
-        int wordInd = word.length - 1;
-        for (int i = post.length - 1; i >= 0; i--) {
-            if (word[wordInd] != post[i]) {
+
+        for (int i = 0; i < post.length; i++) {
+            if (word[word.length + i - 2] != post[i]) {
                 result = false;
                 break;
             }
-            wordInd--;
         }
         return result;
     }
