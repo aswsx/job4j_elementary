d02a1696a01bb35894a292d3e64e5305e1f9e3d3
aswsx <61579000+aswsx@users.noreply.github.com>
2020-08-19 10:39:35 +0600

    6.7.2. Моно столбец в матрице. [#343639]

---
+++ test/ru/job4j/array/Defragment.java
@@ -0,0 +1,28 @@
+package ru.job4j.array;
+
+public class Defragment {
+    public static String[] compress(String[] array) {
+        for (int index = 0; index < array.length; index++) {
+            if (array[index] == null) {
+                for (int i = 0; i < array.length; i++) {
+                    if (array[i] != null) {
+                        array[index] = array[i];
+                        array[i] = null;
+                        break;
+                    }
+                }
+            }
+            System.out.print(array[index] + " ");
+        }
+        return array;
+    }
+
+    public static void main(String[] args) {
+        String[] input = {"I", null, "wanna", null, "be", null, "compressed"};
+        String[] compressed = compress(input);
+        System.out.println();
+        for (int index = 0; index < compressed.length; index++) {
+            System.out.print(compressed[index] + " ");
+        }
+    }
+}
