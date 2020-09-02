8859f676bb01add3a21674ccab517bbe055ba300
aswsx <61579000+aswsx@users.noreply.github.com>
2020-09-02 16:06:45 +0600

    4. Расстояние между точками в трехмерном пространстве. [#343799]

--- src/ru/job4j/condition/Point.java
+++ src/ru/job4j/condition/Point.java
@@ -19,19 +19,28 @@ public class Point {
         this.z = z;
     }
 
+
     public static void main(String[] args) {
-        double result = Point.distance(0, 0, 2, 0);
+        Point secondPoint = new Point(1, 2, 3);
+        double result = 0;
+        result = distance3D(secondPoint);
+        result = Point.distance(0, 0, 2, 0);
         System.out.println("result (0, 0) to (2, 0) " + result);
         result = Point.distance(1, 2, 3, 4);
         System.out.println("result (1, 2) to (3, 4) " + result);
         result = Point.distance(5, 3, 1, 4);
         System.out.println("result (5, 3) to (1, 4) " + result);
     }
+
     public double distance(Point that) {
         return sqrt(pow((this.x - that.x), 2) + pow((this.y - that.y), 2));
     }
 
-    public double distance3D(Point that) {
+    public static double distance(int x1, int y1, int x2, int y2) {
+        return Math.sqrt(Math.pow((x2 - x1), 2) + Math.pow((y2 - y1), 2));
+    }
+
+    public static double distance3D(Point that) {
         return sqrt(pow((this.x - that.x), 2) + pow((this.y - that.y), 2) + pow((this.z - that.z), 2));
     }
 
--- test/ru/job4j/condition/PointTest.java
+++ test/ru/job4j/condition/PointTest.java
@@ -25,4 +25,18 @@ public class PointTest {
         double out = Point.distance(x1In, y1In, x2In, y2In);
         Assert.assertEquals(expected, out, 0.01);
     }
+
+    @Test
+    public void distance3D() {
+        int x1In = 0;
+        int y1In = 0;
+        int z1In = 0;
+        int x2In = 1;
+        int y2In = 3;
+        int z2In = 3;
+        double expected = 4.35;
+        double out = Point.distance3D(x1In,y1In,z1In, x2In, y2In, z2In);
+        Assert.assertEquals(expected, out, 0.01);
+        }
+    }
 }
