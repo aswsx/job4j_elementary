b583951ab4c172c142f333f5020871376830cd54
Alex <lextor78@gmail.com>
2020-09-10 13:16:21 +0600

    6.0.1.1. Размер массива [#343652]

--- src/ru/job4j/array/ArrayDefinition.java
+++ src/ru/job4j/array/ArrayDefinition.java
@@ -1,12 +1,15 @@
 package ru.job4j.array;
 
 public class ArrayDefinition {
-    short[] ages = new short[10];
-    String[] surnames = new String[100500];
-    float[] prices = new float[40];
+    static short[] ages = new short[10];
+    static String[] surnames = new String[100500];
+    static float[] prices = new float[40];
 
     @SuppressWarnings("checkstyle:EmptyLineSeparator")
     public static void main(String[] args) {
+        System.out.println("ages length = " + ages.length);
+        System.out.println("surnames length = " + surnames.length);
+        System.out.println("prices length = " + prices.length);
         String[] names = new String[4];
         names[0] = "Vasya";
         names[1] = "Fedya";
