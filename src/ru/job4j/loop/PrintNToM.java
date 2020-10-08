ac619646df84aa8350e12fd134e8dd66dc990cf4
Alex <lextor78@gmail.com>
2020-10-08 23:01:27 +0600

    2. Вывести на консоль числа от N до M

---
+++ src/ru/job4j/loop/PrintNToM.java
@@ -0,0 +1,9 @@
+package ru.job4j.loop;
+
+public class PrintNToM {
+    public static void out(int n, int m) {
+        for (int i = n; i < m; i++) {
+            System.out.println(i);
+        }
+    }
+}
