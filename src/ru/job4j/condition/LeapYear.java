e82e53b593992cf3a97a6676de46364e3350a6a0
Alex <lextor78@gmail.com>
2020-10-08 21:40:29 +0600

    9. Високосный год.

---
+++ src/ru/job4j/condition/LeapYear.java
@@ -0,0 +1,13 @@
+package ru.job4j.condition;
+
+public class LeapYear {
+    public static boolean checkYear(int year) {
+        if (year % 400 == 0) {
+            return true;
+        } else if (year % 100 == 0) {
+            return false;
+        } else {
+            return year % 4 == 0;
+        }
+    }
+}
