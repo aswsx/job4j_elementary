48e04fc67ed43ba04d5ad8ea9d71767b8e31b2e8
Alex <lextor78@gmail.com>
2020-10-08 21:28:29 +0600

    8. Hello World

---
+++ src/ru/job4j/condition/HelloWorld.java
@@ -0,0 +1,14 @@
+package ru.job4j.condition;
+
+public class HelloWorld {
+    public static String checkNumber(int number) {
+        if (number % 3 == 0 && number % 5 == 0) {
+            return "Hello, World!!!";
+        } else if (number % 3 == 0) {
+            return "Hello";
+        } else if (number % 5 == 0) {
+            return "World";
+        }
+        return "Operation not support";
+    }
+}
