553dfb98128bc0c82d2357a93fd5f0bc990db869
aswsx <61579000+aswsx@users.noreply.github.com>
2020-10-09 09:52:48 +0600

    9.4. mod через while

---
+++ src/ru/job4j/loop/ModWithLoop.java
@@ -0,0 +1,11 @@
+package ru.job4j.loop;
+
+public class ModWithLoop {
+    public static int mod(int n, int d) {
+        int rsl = 0;
+        while(n >= d) {
+            n = n - d;
+            rsl = n;
+        }return rsl;
+    }
+}
