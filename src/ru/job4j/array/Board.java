a45be3b5fdb27b8a18850cad1d868854068e9097
Alex <lextor78@gmail.com>
2020-10-14 23:10:54 +0600

    9. Размер двумерного массива

---
+++ src/ru/job4j/array/Board.java
@@ -0,0 +1,11 @@
+package ru.job4j.array;
+
+public class Board {
+    public static int getCountRow(int[][] array) {
+        return array.length;
+    }
+
+    public static int getCountCellInRow(int[][] array, int row) {
+        return array[2].length;
+    }
+}
