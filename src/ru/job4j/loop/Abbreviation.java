8ec3c5d61cfd44942dfd46a19ec3a3f3993dfbc9
Alex <lextor78@gmail.com>
2020-10-14 22:26:34 +0600

    13. Аббревиатуры

---
+++ src/ru/job4j/loop/Abbreviation.java
@@ -0,0 +1,13 @@
+package ru.job4j.loop;
+
+public class Abbreviation {
+    public static String collect(String s) {
+        StringBuilder str = new StringBuilder();
+        for (int i = 0; i < s.length() - 1; i++) {
+            if (Character.isUpperCase(s.charAt(i))) {
+                str.append(s.charAt(i));
+            }
+        }
+        return str.toString();
+    }
+}
