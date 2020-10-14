46261b68635999acc9982994364f4dfa8fecc88d
Alex <lextor78@gmail.com>
2020-10-14 23:08:01 +0600

    8. Индекс максимального значения

---
+++ src/ru/job4j/array/IndexMaxElement.java
@@ -0,0 +1,7 @@
+package ru.job4j.array;
+
+public class IndexMaxElement {
+    public static int getIndexMax(int[] array) {
+        return array[0] > array[array.length - 1] ? 0 : array.length - 1;
+    }
+}
