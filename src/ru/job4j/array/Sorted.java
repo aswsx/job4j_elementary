2f4df99084e67e5771b8f638f392f84df4cab5fe
Alex <lextor78@gmail.com>
2020-10-14 22:33:53 +0600

    1. Проверить сортировку массива

---
+++ src/ru/job4j/array/Sorted.java
@@ -0,0 +1,12 @@
+package ru.job4j.array;
+
+public class Sorted {
+    public static boolean isSorted(int[] array) {
+        for (int i = 0; i < array.length - 1; i++) {
+            if (array[i] > array[i + 1]) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
