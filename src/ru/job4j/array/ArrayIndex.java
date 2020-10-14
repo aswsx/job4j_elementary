6165e0a644cee34eda6e4ca12f11e2da52e02d7b
Alex <lextor78@gmail.com>
2020-10-14 22:57:43 +0600

    5. Индексы в массивах

---
+++ src/ru/job4j/array/ArrayIndex.java
@@ -0,0 +1,12 @@
+package ru.job4j.array;
+
+public class ArrayIndex {
+
+    public static int getFirstElement(int[] array) {
+        return array[0];
+    }
+
+    public static int getLastElement(int[] array) {
+        return array[array.length - 1];
+    }
+}
