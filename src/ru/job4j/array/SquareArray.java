716930d668b21d97074fadc561a2cfe90fa0c7b1
Alex <lextor78@gmail.com>
2020-10-14 23:15:03 +0600

    10. Создание квадратного двумерного массива

---
+++ src/ru/job4j/array/SquareArray.java
@@ -0,0 +1,12 @@
+package ru.job4j.array;
+
+public class SquareArray {
+    public static void main(String[] args) {
+        int[][] array = new int[][]{{1, 2}, {3, 4}};
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array.length; j++) {
+                System.out.println(array[i][j]);
+            }
+        }
+    }
+}
