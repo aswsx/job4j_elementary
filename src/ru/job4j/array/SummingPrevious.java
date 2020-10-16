acf5eb5212ba21c9197e082b0b6a1451b778bec1
Alex <lextor78@gmail.com>
2020-10-16 19:36:04 +0600

    18. Суммирования предыдущих значений

--- src/ru/job4j/array/SummingPrevious.java
+++ src/ru/job4j/array/SummingPrevious.java
@@ -2,14 +2,14 @@ package ru.job4j.array;
 
 public class SummingPrevious {
     public static int[] calculate(int a, int b, int n) {
-        int[] result = new int[n]; //выходной массив длиной n
+        int[] result = new int[n];
         result[0] = a;
         result[1] = b;
-        int temp = a + b;// переменная в которой суммируем значения элементов
+        int temp = a + b;
 
-        for (int i = 2; i < n; i++) {// проходим циклом по массиву
-            result[i] = temp; //в соответствующий элемент записывам значение temp
-            temp += result[i];// прибавляем к temp текущее значение
+        for (int i = 2; i < n; i++) {
+            result[i] = temp;
+            temp += result[i];
         }
         return result;
     }
