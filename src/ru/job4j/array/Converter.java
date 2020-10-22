2153eb865fccdf6df4ddc6883ab247676b0b5028
Alex <lextor78@gmail.com>
2020-10-22 20:31:45 +0600

    31. Конвертировать неквадратный двумерный массив в квадратный.ver5

--- src/ru/job4j/array/Converter.java
+++ src/ru/job4j/array/Converter.java
@@ -2,7 +2,7 @@ package ru.job4j.array;
 
 public class Converter {
     public static int[][] convertInSquareArray(int[][] array) {
-        int squareArraySize;//размер квадратного массива
+        int squareArraySize; //размер квадратного массива
         int arrayElementQty = 0; //количество элементов массива array
         int arrayLength = array.length; //длина массива array (количество строк)
 //вычисляем количество элементов
@@ -16,21 +16,21 @@ public class Converter {
         if (xxx % 1 == 0) { // если корень целое число
             squareArraySize = (int) xxx; //размер квадратного массива равен корню
         } else {
-            squareArraySize = (int) (xxx + 1);//иначе размер равен корень плюс 1
+            squareArraySize = (int) (xxx + 1); //иначе размер равен корень плюс 1
         }
 //создаем квадратный массив
-        int[][] squareArray = new int[squareArraySize][squareArraySize]; //новый квадратный массив
+        int[][] squareArray = new int[squareArraySize][squareArraySize];
 //создаем линейный массив, заполненный длинами строк массива array, для использования в цикле
-        int[] arQty = new int[arrayLength]; //новый линейный массив, длиной равной количеству строк в array
+        int[] arQty = new int[arrayLength];
         for (int i = 0; i < arrayLength; i++) {
             arQty[i] = array[i].length; //заполняем новый массив длинами строк
         }
-//исходя из длины(кол-ва строк исходного массива array и длины каждой строки читаем элементы из array и
+//исходя из длины (кол-ва строк исходного массива array) и длины каждой строки читаем элементы из array и
 // записываем их в линейный массив
         int index = 0;
         for (int i = 0; i < arrayLength; i++) {
             int size = arQty[i]; //количество проходов второго цикла
-            if (index < linearArray.length - 1) {
+            if (index <= linearArray.length - 1) {
                 for (int j = 0; j < size; j++) {
                     linearArray[index] = array[i][j];
                     index++;
@@ -41,16 +41,19 @@ public class Converter {
         }
 //считываем элементы из линейного массива и записываем их в квадратный массив.
         int ind = 0;
-        for (int i = 0; i < squareArraySize; i++) {
-            for (int j = 0; j < squareArraySize; j++) {
-                squareArray[i][j] = linearArray[ind];
+        while (ind < linearArray.length) {
+            for (int i = 0; i < squareArraySize; i++) {
                 if (ind < linearArray.length - 1) {
-                    ind++;
+                    for (int j = 0; j < squareArraySize; j++) {
+                        squareArray[i][j] = linearArray[ind];
+                        ind++;
+                    }
                 } else {
                     break;
                 }
             }
         }
+
         return squareArray;
     }
 }
