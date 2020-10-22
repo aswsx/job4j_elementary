d6f2bdff1a7afe1afbbb946950869b53a66fdc3a
Alex <lextor78@gmail.com>
2020-10-22 23:16:37 +0600

    31. Конвертировать неквадратный двумерный массив в квадратный.

--- src/ru/job4j/array/Converter.java
+++ src/ru/job4j/array/Converter.java
@@ -2,44 +2,36 @@ package ru.job4j.array;
 
 public class Converter {
     public static int[][] convertInSquareArray(int[][] array) {
-        int squareArraySize; //размер квадратного массива
         int arrayElementQty = 0; //количество элементов массива array
-       //расчет количества элементов
         for (int[] rows : array) {
-            arrayElementQty += rows.length;
+            arrayElementQty += rows.length; //расчет количества элементов
         }
-//создание линейного массива длиной, равной количеству элементов
-        int[] linearArray = new int[arrayElementQty];
-//расчет размера квадратного массива
+        int[] linearArray = new int[arrayElementQty]; //создание линейного массива длиной, равной количеству элементов
+        int index = 0;
+        for (int[] rows : array) { // исходя из длины (кол-ва строк исходного массива array)
+            for (int elem : rows) { // и длины каждой строки, чтение элементов из array и
+                linearArray[index] = elem; // запись их в линейный массив
+                index++;
+            }
+        }
+        int squareArraySize; //размер квадратного массива
         double sqArSize = Math.sqrt(arrayElementQty); // корень из количества элементов
         if (sqArSize % 1 == 0) { // если корень целое число
             squareArraySize = (int) sqArSize; //размер квадратного массива равен корню
         } else {
             squareArraySize = (int) (sqArSize + 1); //иначе размер равен корень плюс 1
         }
-//создание квадратного массива
-        int[][] squareArray = new int[squareArraySize][squareArraySize];
-// исходя из длины (кол-ва строк исходного массива array) и длины каждой строки, чтение элементов из array и
-// запись их в линейный массив
-        int index = 0;
-        for (int[] rows : array) {
-            for (int elem : rows) {
-                linearArray[index] = elem;
-                index++;
-            }
-        }
-//считывание элементов из линейного массива и запись их в квадратный массив.
+        int[][] squareArray = new int[squareArraySize][squareArraySize]; //создание квадратного массива
         int ind = 0;
-        for (int i = 0; i < squareArraySize; i++) {
-            for (int j = 0; j < squareArraySize; j++) {
-                if (ind < linearArray.length) {
-                    squareArray[i][j] = linearArray[ind];
-                    ind++;
-                } else {
-                    break;
+
+        while (ind < linearArray.length) {
+            for (int i = 0; i < squareArraySize; i++) { //считывание элементов из линейного массива
+                for (int j = 0; j < squareArraySize; j++) {
+                        squareArray[i][j] = linearArray[ind]; //и запись их в квадратный массив.
+                        ind++;
+                    }
                 }
             }
-        }
         return squareArray;
     }
 }
