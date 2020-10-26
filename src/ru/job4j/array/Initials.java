404bbd716b3001cfb7f8cb2634dfc18557a496d6
aswsx <61579000+aswsx@users.noreply.github.com>
2020-10-26 22:26:10 +0600

    40. Инициалы

---
+++ src/ru/job4j/array/Initials.java
@@ -0,0 +1,11 @@
+package ru.job4j.array;
+
+public class Initials {
+    public static String convert(String[] fio) {
+        String name = null;
+        for (int i = 0; i < fio.length; i++) {
+            name = fio[0] + " " + fio[1].charAt(0) + '.' + fio[2].charAt(0) + '.';
+        }
+        return name;
+    }
+}
