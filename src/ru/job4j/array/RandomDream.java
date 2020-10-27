501c1ee5aaf8362de658cc6bef723433764f6eef
aswsx <61579000+aswsx@users.noreply.github.com>
2020-10-27 10:24:49 +0600

    44. Поле чудес

---
+++ src/ru/job4j/array/RandomDream.java
@@ -0,0 +1,18 @@
+package ru.job4j.array;
+
+public class RandomDream {
+    public static String random(String[] prizes, int num) {
+        String prize = null;
+        if (num < 4) {
+            num += 4;
+        }
+        int xxx = num % 4;//(int) rnd;
+        switch (xxx) {
+            case 1 -> prize = prizes[0];
+            case 2 -> prize = prizes[1];
+            case 3 -> prize = prizes[2];
+            case 0 -> prize = prizes[3];
+        }
+        return prize;
+    }
+}
