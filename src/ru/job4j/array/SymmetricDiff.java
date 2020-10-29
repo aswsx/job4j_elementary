03646496f973d7cfbf08a385bd1938efd412a3e8
aswsx <61579000+aswsx@users.noreply.github.com>
2020-10-29 23:13:41 +0600

    49. Симметрическая разность множеств

---
+++ src/ru/job4j/array/SymmetricDiff.java
@@ -0,0 +1,43 @@
+package ru.job4j.array;
+
+public class SymmetricDiff {
+    public static int[] diff(int[] left, int[] right) {
+        int[] res = new int[0];
+        if (left.length == 0 && right.length == 0 ||
+                left.length == 1 && right.length == 1 && left[0] == right[0]) {
+            return res;
+        } else if (left.length == 1) {
+            res = new int[1];
+            res[0] = left[0];
+        } else if (right.length == 1) {
+            res = new int[1];
+            res[0] = right[0];
+        } else {
+            int[] merge = new int[left.length + right.length];
+            int k = 0;
+            for (int n : left) {
+                merge[k++] = n;
+            }
+            for (int n : right) {
+                merge[k++] = n;
+            }
+            int len = merge.length;
+            for (int i = 0; i < len - 1; i++) {
+                for (int j = i + 1; j < len; j++) {
+                    if (merge[i] == merge[j]) {
+                        int temp = merge[i];
+                        //       for (int m = 0; m < len; m++) {
+                        //          if (merge[m] == temp) {
+                        merge[i] = merge[i + 2];
+                        len -= 2;
+                    }
+                }
+            }
+            res = new int[len];
+            if (res.length >= 0) {
+                System.arraycopy(merge, 0, res, 0, res.length);
+            }
+        }
+        return res;
+    }
+}
