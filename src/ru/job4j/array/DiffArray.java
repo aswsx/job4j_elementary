385fbdb53c1c4caf7a502007b667c7e03801eb85
aswsx <61579000+aswsx@users.noreply.github.com>
2020-10-29 21:56:43 +0600

    48. Разность множеств

--- src/ru/job4j/array/DiffArray.java
+++ src/ru/job4j/array/DiffArray.java
@@ -1,19 +1,26 @@
 package ru.job4j.array;
 
+import java.util.Arrays;
+
 public class DiffArray {
     public static int[] diff(int[] left, int[] right) {
-        int[] res = new int[left.length + right.length];
+        int[] res = new int[0];
         if (left.length == 0) {
-            res = new int[0];
+            return res;
+        } else if (left.length == 1) {
+            res = new int[1];
+            res[0] = left[0];
         } else {
+            res = new int[left.length + right.length];
             int l = 0;
             for (int k : left) {
-                for (int i = 0; i < right.length - 1; i++) {
-                    if (k != right[i]) {
+                for (int m : right) {
+                    if (k != m) {
                         res[l++] = k;
                     }
                 }
             }
+            res = Arrays.copyOf(res, l);
         }
         return res;
     }
