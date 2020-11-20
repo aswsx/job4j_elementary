42ee025eeefdf23be941d8dac6695b065c2215b7
Alex <lextor78@gmail.com>
2020-11-20 21:18:51 +0600

    0. Set, HashSet, Iterator. [#343761]

--- src/ru/job4j/sort/Machine.java
+++ src/ru/job4j/sort/Machine.java
@@ -3,17 +3,17 @@ package ru.job4j.sort;
 import java.util.Arrays;
 
 public class Machine {
-    private final int[] COINS = {10, 5, 2, 1};//массив с монетами, 4 мешка
+    private final int[] coins = {10, 5, 2, 1}; //массив с монетами, 4 мешка
 
     public int[] change(int money, int price) {
-        int[] rsl = new int[100];// временный массив произвольного размера с запасом
-        int size = 0;// будущий размер конечного массива
-        int index = 0;// счетчик монет
+        int[] rsl = new int[100]; // временный массив произвольного размера с запасом
+        int size = 0; // будущий размер конечного массива
+        int index = 0; // счетчик монет
         int change = money - price; // размер сдачи
-        for (int i = 0; i < COINS.length; i++) { //цикл по мешкам с монетами
-            while (change >= COINS[i]) { //цикл по монетам в  текущем мешке
-                change = change - COINS[i]; // отнимаем от сдачи монету из мешка i
-                rsl[index] = COINS[i]; // добавляем монету во временный массив
+        for (int i = 0; i < coins.length; i++) { //цикл по мешкам с монетами
+            while (change >= coins[i]) { //цикл по монетам в  текущем мешке
+                change = change - coins[i]; // отнимаем от сдачи монету из мешка i
+                rsl[index] = coins[i]; // добавляем монету во временный массив
                 size = index + 1; //увеличиваем размер конечного массива на 1
                 index++; //увеличиваем счетчик монет на 1
             }
