16d915195b6e37b64ad43abc170a8ec4b4df2fdf
Alex <lextor78@gmail.com>
2020-11-23 21:01:57 +0600

    4. Найти индекс элемента

---
+++ src/ru/job4j/list/UniqueElement.java
@@ -0,0 +1,12 @@
+package ru.job4j.list;
+
+import java.util.List;
+
+public class UniqueElement {
+    public static boolean checkList(List<String> list, String str) {
+        if (!(list.contains(str))) {
+            return false;
+        }
+        return list.indexOf(str) == list.lastIndexOf(str);
+    }
+}
