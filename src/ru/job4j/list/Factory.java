bfdd815a136aeace147c250ef5c24d7a2889ec01
Alex <lextor78@gmail.com>
2020-11-23 21:36:46 +0600

    5. Заполнение списка фабричным методом

--- src/ru/job4j/list/Factory.java
+++ src/ru/job4j/list/Factory.java
@@ -5,8 +5,8 @@ import java.util.List;
 public class Factory {
     public static void main(String[] args) {
         List<String> list = List.of("first", "second", "third", "fourth", "fifth");
-        for (int i = 0; i < list.size(); i++) {
-            System.out.println(list.get(i));
+        for (String s : list) {
+            System.out.println(s);
         }
     }
 }
