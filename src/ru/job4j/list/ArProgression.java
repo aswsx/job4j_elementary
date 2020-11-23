6ddfefea3e4473cff93ecea42fc81ae5eedf869d
Alex <lextor78@gmail.com>
2020-11-23 23:00:07 +0600

    14. Арифметическая прогрессия

---
+++ src/ru/job4j/list/ArProgression.java
@@ -0,0 +1,16 @@
+package ru.job4j.list;
+
+import java.util.List;
+
+public class ArProgression {
+    public static int checkData(List<Integer> data) {
+        int sum = data.get(0);
+        for (int i = 1; i < data.size() - 1; i++) {
+            if (data.get(i) != (data.get(i - 1) + data.get(i + 1)) / 2) {
+                return 0;
+            }
+            sum += data.get(i);
+        }
+        return sum += data.get(data.size() - 1);
+    }
+}
