245aa512c09d918315fe5e230725123d3154374d
Alex <lextor78@gmail.com>
2020-11-24 00:41:40 +0600

    OrArray Исправление ошибок№ Переписал заново

--- src/ru/job4j/array/OrArray.java
+++ src/ru/job4j/array/OrArray.java
@@ -1,54 +1,29 @@
 package ru.job4j.array;
 
+import java.util.Arrays;
+
 public class OrArray {
     public static int[] or(int[] left, int[] right) {
-        int[] merge = mergeIt(left, right);
-        int[] remove = removeIt(merge);
-        return sortIt(remove);
+        int[] rsl = new int[left.length + right.length];
+        int size = add(rsl, left, 0);
+        size = add(rsl, right, size);
+        return Arrays.copyOf(rsl, size);
     }
 
-    private static int[] mergeIt(int[] left, int[] right) {
-        int[] merge = new int[left.length + right.length];
-        int k = 0;
-        for (int n : left) {
-            merge[k++] = n;
-        }
-        for (int n : right) {
-            merge[k++] = n;
-        }
-        return merge;
-    }
-
-    private static int[] removeIt(int[] merge) {
-        int len = merge.length;
-        for (int i = 0; i < len - 1; i++) {
-            for (int j = i + 1; j < len; j++) {
-                if (merge[i] == merge[j]) {
-                    for (int k = j; k < len - 1; k++) {
-                        merge[k] = merge[k + 1];
-                        --len;
-                    }
+    private static int add(int[] rsl, int[] array, int size) {
+        for (int k : array) {
+            boolean needAdd = true;
+            for (int i : rsl) {
+                if (i == k) {
+                    needAdd = false;
+                    break;
                 }
             }
-        }
-
-        int[] remove = new int[len];
-        for (int i = 0; i < remove.length; i++) {
-            remove[i] = merge[i];
-        }
-        return remove;
-    }
-
-    private static int[] sortIt(int[] remove) {
-        for (int index = 0; index < remove.length - 1; index++) {
-            for (int i = index + 1; i < remove.length; i++) {
-                if (remove[index] > remove[i]) {
-                    int temp = remove[index];
-                    remove[index] = remove[i];
-                    remove[i] = temp;
-                }
+            if (needAdd) {
+                rsl[size] = k;
+                size++;
             }
         }
-        return remove;
+        return size;
     }
 }
