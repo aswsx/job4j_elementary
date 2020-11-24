592bfd0ab09d6e81d1519706dfdafd6d4589d96f
aswsx <61579000+aswsx@users.noreply.github.com>
2020-11-24 23:40:01 +0600

    Замена switch

--- src/main/java/ru/job4j/condition/MultipleSwitchWeek.java
+++ src/main/java/ru/job4j/condition/MultipleSwitchWeek.java
@@ -2,23 +2,23 @@ package ru.job4j.condition;
 
 public class MultipleSwitchWeek {
     public int numberOfDay(String name) {
-        switch (name) {
-            case "Понедельник", "Monday":
-                return 1;
-            case "Вторник", "Tuesday":
-                return 2;
-            case "Среда", "Wednesday":
-                return 3;
-            case "Четверг", "Thursday":
-                return 4;
-            case "Пятница", "Friday":
-                return 5;
-            case "Суббота", "Sunday":
-                return 6;
-            case "Воскресенье", "Saturday":
-                return 7;
-            default:
-                return -1;
+
+        if (name.equals("Понедельник") || name.equals("Monday")) {
+            return 1;
+        } else if (name.equals("Вторник") || name.equals("Tuesday")) {
+            return 2;
+        } else if (name.equals("Среда") || name.equals("Wednesday")) {
+            return 3;
+        } else if (name.equals("Четверг") || name.equals("Thursday")) {
+            return 4;
+        } else if (name.equals("Пятница") || name.equals("Friday")) {
+            return 5;
+        } else if (name.equals("Суббота") || name.equals("Sunday")) {
+            return 6;
+        } else if (name.equals("Воскресенье") || name.equals("Saturday")) {
+            return 7;
+        } else {
+            return -1;
         }
     }
 }
