490c11a59b7a0ce3aa4cd7ff92f2b26841e403c1
aswsx <61579000+aswsx@users.noreply.github.com>
2020-12-01 22:02:16 +0600

    yml

--- src/main/java/ru/job4j/set/SetIterator.java
+++ src/main/java/ru/job4j/set/SetIterator.java
@@ -7,10 +7,9 @@ import java.util.TreeSet;
 public class SetIterator {
     public static void main(String[] args) {
         Set<String> strings = new TreeSet<>(Set.of("one", "two", "three", "four", "five"));
-        Iterator<String> iterator = strings.iterator();
-        while (iterator.hasNext()) {
-            System.out.println(String.format("Next element exist? - %s.",true));
-            System.out.println(iterator.next());
+        for (String string : strings) {
+            System.out.printf("Next element exist? - %s.%n", true);
+            System.out.println(string);
         }
     }
 }
