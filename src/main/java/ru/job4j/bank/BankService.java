d62119e565657464ad5a78684cbfd0df34baba4c
Alex <lextor78@gmail.com>
2021-01-04 13:36:21 +0600

    6. Тестовое задание из модуля коллекции Lite переделать на Stream API. [#343581]

--- src/main/java/ru/job4j/bank/BankService.java
+++ src/main/java/ru/job4j/bank/BankService.java
@@ -3,19 +3,21 @@ package ru.job4j.bank;
 import java.util.*;
 
 /**
- *Класс описывает работу простого банковского сервиса
+ * Класс описывает работу простого банковского сервиса
  * Содержит базу клиентов с их счетами.
  * Позволяет найти клиента по пасспорту, по номеру счета,
  * добавить клиента, перевести деньги со счета на счет
+ *
  * @author Alexey Gutorov
  * @version 1.0
  */
 public class BankService {
-    private Map<User, List<Account>> users = new HashMap<>();
+    private final Map<User, List<Account>> users = new HashMap<>();
 
     /**
      * Метод принимает клиента на вход и добавляет его в базу
      * если его еще нет в базе
+     *
      * @param user клиент? добавляемый в базу
      */
     public void addUser(User user) {
@@ -24,8 +26,9 @@ public class BankService {
 
     /**
      * Метод добавляет клиенту счет? если такого еще нет
+     *
      * @param passport паспорт клиента к которому привязывается счет
-     * @param account добавляемый счет
+     * @param account  добавляемый счет
      */
     public void addAccount(String passport, Account account) {
         User user = findByPassport(passport);
@@ -41,48 +44,66 @@ public class BankService {
 
     /**
      * Метод находит клиента по паспорту
+     *
      * @param passport паспорт клиента, принимаемый на вход
      * @return возвращает найденного пользователя или null, если клиент не найден
      */
     public User findByPassport(String passport) {
-        User user = null;
-        for (User key : users.keySet()) {
-            if (key.getPassport().equals(passport)) {
-                user = key;
-                break;
-            }
-        }
-        return user;
+        return users.keySet()
+                .stream()
+                .filter(key -> key.getPassport().equals(passport))
+                .findFirst()
+                .orElse(null);
     }
+//        User user = null;
+//        for (User key : users.keySet()) {
+//            if (key.getPassport().equals(passport)) {
+//                user = key;
+//                break;
+//            }
+//        }
+//        return user;
 
     /**
      * Метод находит клиента по счету
-     * @param passport паспорт, принимаемый на вход
+     *
+     * @param passport  паспорт, принимаемый на вход
      * @param requisite искомый номер счета
      * @return возвращает клиента или null, если клиент не найден
      */
     public Account findByRequisite(String passport, String requisite) {
-        Account rslAccount = null;
         User user = findByPassport(passport);
-        List<Account> accounts = users.get(user);
-        if (accounts != null) {
-            for (Account account : accounts) {
-                if (account.getRequisite().equals(requisite)) {
-                    rslAccount = account;
-                    break;
-                }
-            }
+        if (user != null) {
+            return users.get(user)
+                    .stream()
+                    .filter(s -> s.getRequisite().equals(requisite))
+                    .findFirst()
+                    .orElse(null);
         }
-        return rslAccount;
+        return null;
     }
+//        Account rslAccount = null;
+//        User user = findByPassport(passport);
+//        List<Account> accounts = users.get(user);
+//        if (accounts != null) {
+//            for (Account account : accounts) {
+//                if (account.getRequisite().equals(requisite)) {
+//                    rslAccount = account;
+//                    break;
+//                }
+//            }
+//        }
+//        return rslAccount;
+//   }
 
     /**
      * Метод переводит деньги между счетами
-     * @param srcPasspord принимаемый на вход паспорт отправителя
-     * @param srcRequisite принимаемый на вход счет отправителя
-     * @param destPassport принимаемый на вход пасспорт получателя
+     *
+     * @param srcPasspord   принимаемый на вход паспорт отправителя
+     * @param srcRequisite  принимаемый на вход счет отправителя
+     * @param destPassport  принимаемый на вход пасспорт получателя
      * @param destRequisite принимаемый на вход счет получателя
-     * @param amount принимаемая на вход сумма
+     * @param amount        принимаемая на вход сумма
      * @return метод возвращает true, если перевод был успешен или false в случае
      * неудачного перевода
      */
