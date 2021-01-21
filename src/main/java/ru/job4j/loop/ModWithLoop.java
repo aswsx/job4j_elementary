5d2d54173a519ced7632979f719983f04b9b0fb2
aswsx <61579000+aswsx@users.noreply.github.com>
2021-01-22 01:29:28 +0600

    corr

--- src/main/java/ru/job4j/loop/ModWithLoop.java
+++ src/main/java/ru/job4j/loop/ModWithLoop.java
@@ -2,11 +2,9 @@ package ru.job4j.loop;
 
 public class ModWithLoop {
     public static int mod(int n, int d) {
-        int rsl = 0;
         while (n >= d) {
-            n = n - d;
-            rsl = n;
+            n -= d;
         }
-        return rsl;
+        return n;
     }
 }
