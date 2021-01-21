9e4fe7404cbca2366debb529e0920c095439cae8
aswsx <61579000+aswsx@users.noreply.github.com>
2021-01-22 01:32:17 +0600

    corr

--- src/main/java/ru/job4j/array/Defragment.java
+++ src/main/java/ru/job4j/array/Defragment.java
@@ -2,7 +2,7 @@ package ru.job4j.array;
 
 public class Defragment {
     public static String[] compress(String[] array) {
-       // for (int index = 0; index < array.length; index++) {
+        // for (int index = 0; index < array.length; index++) {
 //            if (array[index] == null) {
 //                for (int i = index; i < array.length; i++) {
 //                    if (array[i] != null) {
@@ -20,17 +20,17 @@ public class Defragment {
 //                }
 //            }
 
-            for (int i = 0, j = 0; j < array.length ; j++) {
-                if (array[j] != null) {
-                    if (i < j) {
-                        String tmp = array[i];
-                        array[i] = array[j];
-                        array[j] = tmp;
-                    }
-                    i++;
+        for (int i = 0, j = 0; j < array.length; j++) {
+            if (array[j] != null) {
+                if (i < j) {
+                    String tmp = array[i];
+                    array[i] = array[j];
+                    array[j] = tmp;
                 }
+                i++;
             }
-       //    System.out.print(array[index] + " ");
+        }
+        //    System.out.print(array[index] + " ");
 
         return array;
     }
