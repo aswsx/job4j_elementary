9f99c1d6cb9d38d236280eb03544a437e03de02b
Tosh-PC <lextor78@gmail.com>
2021-03-03 17:40:22 +0600

    corr

--- src/main/java/ru/job4j/array/AlgoArray.java
+++ src/main/java/ru/job4j/array/AlgoArray.java
@@ -2,7 +2,7 @@ package ru.job4j.array;
 
 public class AlgoArray {
     public static void main(String[] args) {
-        int[] array = new int[] {5, 3, 2, 1, 4};
+        int[] array = new int[]{5, 3, 2, 1, 4};
         int temp = array[0];
         array[0] = array[3];
         array[3] = temp;
--- src/main/java/ru/job4j/ex/FindEl.java
+++ src/main/java/ru/job4j/ex/FindEl.java
@@ -1,26 +1,28 @@
 package ru.job4j.ex;
 
-public class FindEl  extends Exception {
+public class FindEl extends Exception {
     public static int indexOf(String[] value, String key) throws ElementNotFoundException {
         int rsl = -1;
-        for (int i = 0; i < value.length; i++) {
-            if (value[i].equals(key)) {
-                rsl = i;
+        int count = 0;
+        for (String i : value) {
+            if (i.equals(key)) {
+                rsl = count;
                 break;
             }
+            count++;
         }
-        if (rsl == -1) {
-            throw new ElementNotFoundException("Элемент не найден");
+            if (rsl == -1) {
+                throw new ElementNotFoundException("Элемент не найден");
+            }
+            return rsl;
         }
-        return rsl;
-    }
 
-    public static void main(String[] args) {
-        String[] value = {"1", "2", "3", "4", "5"};
-        try {
-            indexOf(value, "");
-        } catch (ElementNotFoundException e) {
-            e.printStackTrace();
+        public static void main (String[]args){
+            String[] value = {"1", "2", "3", "4", "5"};
+            try {
+                indexOf(value, "");
+            } catch (ElementNotFoundException e) {
+                e.printStackTrace();
+            }
         }
     }
-}
