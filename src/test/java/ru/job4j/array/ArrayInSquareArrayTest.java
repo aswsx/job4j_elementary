c4770530ed7156b494bdf2eb9fc397f4b7133732
Tosh-PC <lextor78@gmail.com>
2021-03-11 22:44:27 +0600

    corr

--- src/test/java/ru/job4j/array/ArrayInSquareArrayTest.java
+++ src/test/java/ru/job4j/array/ArrayInSquareArrayTest.java
@@ -1,6 +1,6 @@
 package ru.job4j.array;
 
-import org.junit.Test;
+import org.testng.annotations.Test;
 
 import static org.hamcrest.core.Is.is;
 import static org.hamcrest.MatcherAssert.assertThat;
