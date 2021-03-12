57f27ceadcad0856dd17d53998f83de8951eaad8
Tosh-PC <lextor78@gmail.com>
2021-03-12 13:03:09 +0600

    corr

--- src/test/java/ru/job4j/array/ArrayInSquareArrayTest.java
+++ src/test/java/ru/job4j/array/ArrayInSquareArrayTest.java
@@ -1,12 +1,12 @@
 package ru.job4j.array;
 
-import org.testng.annotations.Test;
+import org.junit.Test;
 
-import static org.hamcrest.core.Is.is;
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.is;
 
 public class ArrayInSquareArrayTest {
-    @Test
+    @org.junit.Test
     public void convertArray() {
         int[] array = {1, 2, 3, 4, 5, 6, 7};
         int[][] expected = {{1, 2, 3}, {4, 5, 6}, {7, 0, 0}};
