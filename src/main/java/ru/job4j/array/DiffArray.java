a77d9215c83646419e2b9ade96e811f164636088
Tosh-PC <lextor78@gmail.com>
2021-03-30 00:41:56 +0600

    corr

--- src/main/java/ru/job4j/array/DiffArray.java
+++ src/main/java/ru/job4j/array/DiffArray.java
@@ -4,23 +4,23 @@ import java.util.Arrays;
 
 public class DiffArray {
     public static int[] diff(int[] left, int[] right) {
-        int[] res;
+        int[] rsl = new int[left.length];
+        int count = 0;
         if (left.length == 0) {
             return new int[0];
         } else if (right.length == 0) {
             return left;
         } else {
-           int[] rsl = new int[left.length];
-            int count = 0;
             for (int k : left) {
                 for (int m : right) {
                     if (k != m) {
                         rsl[count++] = k;
+                    } else {
+                        break;
                     }
                 }
             }
-            res = Arrays.copyOf(rsl, count);
         }
-        return res;
+        return Arrays.copyOf(rsl, count);
     }
 }
