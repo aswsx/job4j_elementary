75b0ba02b5655d73e38ff26a9f4e380b8b085171
Tosh-PC <lextor78@gmail.com>
2021-04-01 13:04:01 +0600

    corr

--- src/main/java/ru/job4j/array/RandomDream.java
+++ src/main/java/ru/job4j/array/RandomDream.java
@@ -2,20 +2,6 @@ package ru.job4j.array;
 
 public class RandomDream {
     public static String random(String[] prizes, int num) {
-        String prize = null;
-        if (num < 4) {
-            num += 4;
-        }
-        int xxx = num % 4;
-        if (xxx == 1) {
-            prize = prizes[0];
-        } else if (xxx == 2) {
-            prize = prizes[1];
-        } else if (xxx == 3) {
-            prize = prizes[2];
-        } else if (xxx == 0) {
-            prize = prizes[3];
-        }
-        return prize;
+        return prizes[num % 4 == 0 ? 3 : num % 4 - 1];
     }
 }
