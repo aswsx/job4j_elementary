7a69479313f6dc64658625419760f91f08a7e38c
Tosh-PC <lextor78@gmail.com>
2021-04-09 01:04:07 +0600

    corr

--- src/main/java/ru/job4j/array/MaxLengthSeria.java
+++ src/main/java/ru/job4j/array/MaxLengthSeria.java
@@ -2,24 +2,16 @@ package ru.job4j.array;
 
 public class MaxLengthSeria {
     public static int find(int[] array) {
-        int maxSeqLen = 0;
-        int curSeqLen = 0;
-        int len = array.length;
+        int length = 1;
 
-        if (len == 2 && array[0] < array[1]) {
-            maxSeqLen = 2;
-        } else if (len == 1 || len == 2) {
-            maxSeqLen = 1;
-        } else {
-            for (int i = 0; i < len - 1; i++) {
-                if (array[i + 1] >= array[i]) {
-                    ++curSeqLen;
-                } else {
-                    maxSeqLen = 1;
-                }
+        for (int i = 0; i < array.length - 1; i++) {
+            if (array[i + 1] >= array[i]) {
+                length++;
+            } else {
+                length = 1;
             }
         }
-        return Math.max(maxSeqLen, curSeqLen);
+        return length;
     }
 }
 
