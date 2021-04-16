36225868de736663d5fdc7ae86eecf3b8dd6eb40
Tosh-PC <lextor78@gmail.com>
2021-04-16 14:54:29 +0600

    corr

--- src/main/java/ru/job4j/array/UpperCase.java
+++ src/main/java/ru/job4j/array/UpperCase.java
@@ -4,11 +4,7 @@ public class UpperCase {
     public static char[] toUpperCase(char[] string) {
         char[] out = new char[string.length];
         for (int i = 0; i < string.length; i++) {
-            if (!Character.isLowerCase(string[i])) {
-                out[i] = string[i];
-            } else {
-                out[i] = Character.toUpperCase(string[i]);
-            }
+            out[i] = Character.toUpperCase(string[i]);
         }
         return out;
     }
