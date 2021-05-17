914f65799767fb01dea6186801764bc415137b51
Tosh-PC <lextor78@gmail.com>
2021-05-18 00:08:51 +0600

    var

--- src/main/java/ru/job4j/loop/Hamming.java
+++ src/main/java/ru/job4j/loop/Hamming.java
@@ -2,7 +2,8 @@ package ru.job4j.loop;
 
 public class Hamming {
     public static int checkStrings(String left, String right) {
-        int hamming = 0;
+        
+        var hamming = 0;
         for (int i = 0; i < left.length(); i++) {
             if (left.charAt(i) != right.charAt(i)) {
                 hamming++;
