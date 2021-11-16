ce841cd82c1df45298f1aa65235fd443fc7bf89a
Tosh-PC <lextor78@gmail.com>
2021-11-16 17:00:48 +0600

    Исправления

--- src/test/java/ru/job4j/array/MaxLengthSeriaTest.java
+++ src/test/java/ru/job4j/array/MaxLengthSeriaTest.java
@@ -8,14 +8,14 @@ public class MaxLengthSeriaTest {
 
     @Test
     public void test() {
-        assertEquals(0, MaxLengthSeria.find(new int[] {}));
-        assertEquals(1, MaxLengthSeria.find(new int[] {1}));
-        assertEquals(1, MaxLengthSeria.find(new int[] {2, 1}));
-        assertEquals(2, MaxLengthSeria.find(new int[] {1, 2}));
-        assertEquals(4, MaxLengthSeria.find(new int[] {1, 2, 0, 1, 2, 3}));
-        assertEquals(4, MaxLengthSeria.find(new int[] {1, 2, 1, 1, 2, 3}));
-        assertEquals(1, MaxLengthSeria.find(new int[] {3, 2, 1}));
-        assertEquals(9, MaxLengthSeria.find(new int[] {6,7,8,9,10,11,1,11,12,13,14,15,16,17,18,2,3,5,2,6,3}));
+        assertEquals(0, MaxLengthSeria.find(new int[]{}));
+        assertEquals(1, MaxLengthSeria.find(new int[]{1}));
+        assertEquals(1, MaxLengthSeria.find(new int[]{2, 1}));
+        assertEquals(2, MaxLengthSeria.find(new int[]{1, 2}));
+        assertEquals(4, MaxLengthSeria.find(new int[]{1, 2, 0, 1, 2, 3}));
+        assertEquals(4, MaxLengthSeria.find(new int[]{1, 2, 1, 1, 2, 3}));
+        assertEquals(1, MaxLengthSeria.find(new int[]{3, 2, 1}));
+        assertEquals(9, MaxLengthSeria.find(new int[]{6, 7, 8, 9, 10, 11, 1, 11, 12, 13, 14, 15, 16, 17, 18, 2, 3, 5, 2, 6, 3}));
     }
 
 }
