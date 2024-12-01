621d0c3113d850c736c8b3cc4a742580d2b4c427
Tosh-PC <lextor78@gmail.com>
2024-12-01 16:24:48 +0500

    6. Правила Checkstyle [#504981]

---
+++ src/main/java/ru/job4j/chekstyle/Broken.java
@@ -0,0 +1,26 @@
+package ru.job4j.chekstyle;
+
+public class Broken {
+    private int sizeOfEmpty = 10;
+
+    private String surname;
+
+    public static final String NEWVALUE = "";
+
+    private String name;
+
+    void echo() {
+    }
+
+    void broken() {
+    }
+
+    void media(Object obj) {
+        if (obj != null) {
+            System.out.println(obj);
+        }
+    }
+
+    void method(int a, int b, int c, int d, int e, int f, int g) {
+    }
+}
