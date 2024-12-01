dd7d2a25a479bd8aa7f2521a498d328ffb7bf104
Tosh-PC <lextor78@gmail.com>
2024-12-01 10:59:22 +0500

    Corrected

--- src/main/java/ru/job4j/array/SortSelectedByGPT.java
+++ src/main/java/ru/job4j/array/SortSelectedByGPT.java
@@ -1,12 +1,16 @@
 package ru.job4j.array;
 
 public class SortSelectedByGPT {
-    public static int[] sort(int[] data) {
-        for (int i = 0; i < data.length; i++) {
-            int minIndex = MinDiapason.findMin(data, i, data.length - 1);
-            SwitchArray.swap(data, minIndex, i);
+    public class SortSelected {
+        public static int[] sort(int[] data) {
+            int[] sorted = new int[data.length];
+            for (int i = 0; i < data.length; i++) {
+                int min = MinDiapason.findMin(data, i, data.length - 1);
+                int index = FindLoop.indexOf(data, min, i, data.length - 1);
+                sorted = SwitchArray.swap(data, index, i);
+            }
+            return sorted;
         }
-        return data;
     }
 }
 
