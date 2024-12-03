7e5510c31941a57f73e7f6651a248cd6a4c26988
Tosh-PC <lextor78@gmail.com>
2024-12-03 20:08:21 +0500

    corr

--- src/main/java/ru/job4j/chekstyle/Broken.java
+++ src/main/java/ru/job4j/chekstyle/Broken.java
@@ -9,18 +9,18 @@ public class Broken {
 
     private String name;
 
-    void echo() {
+    public Broken() {
     }
 
-    void broken() {
+    public void echo() {
     }
 
-    void media(Object obj) {
+    public void media(Object obj) {
         if (obj != null) {
             System.out.println(obj);
         }
     }
 
-    void method(int a, int b, int c, int d, int e, int f, int g) {
+    public void method(int a, int b, int c, int d, int e, int f, int g) {
     }
 }
