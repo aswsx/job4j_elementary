0c5e038618e87b1799033d087e22537e03079627
Tosh-PC <lextor78@gmail.com>
2024-12-03 20:13:39 +0500

    corr

--- src/main/java/ru/job4j/array/Initials.java
+++ src/main/java/ru/job4j/array/Initials.java
@@ -2,10 +2,11 @@ package ru.job4j.array;
 
 public class Initials {
     public static String convert(String[] fio) {
-        /**   // String name = null;
-         //   for (int i = 0; i < fio.length; i++) {
-         //   name =
-         //  }
+        /**
+         String name = null;
+         for (int i = 0; i < fio.length; i++) {
+         name =
+         }
          return fio[0] + " " + fio[1].charAt(0) + '.' + fio[2].charAt(0) + '.';
          */
         return "";
